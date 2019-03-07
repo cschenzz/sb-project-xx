9bd2467a9a237737826051f68eb657da6ba5d221
chenzz <cs.chenzz@qq.com>
2019-03-07 12:07:59 +0800

    修改数据库脚本,配置等

--- doc/update20181227.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `sibo-gxy-2`.`sys_dict_type` (`dict_id`, `dict_name`, `dict_type`, `status`, `create_by`, `create_time`, `update_by`, `update_time`, `remark`) VALUES ('13', '报警触发', 'SOS_trigger', '0', 'sibo-chenzz', '2018-12-27 19:43:02', 'sibo-chenzz', '2018-12-27 19:44:18', '报警触发');
-INSERT INTO `sibo-gxy-2`.`sys_dict_data` (`dict_code`, `dict_sort`, `dict_label`, `dict_value`, `dict_type`, `list_class`, `is_default`, `status`) VALUES ('36', '1', '180', 'SOS_hight', 'SOS_trigger', 'default', 'N', '0');
-INSERT INTO `sibo-gxy-2`.`sys_dict_data` (`dict_code`, `dict_sort`, `dict_label`, `dict_value`, `dict_type`, `list_class`, `is_default`, `status`) VALUES ('37', '2', '50', 'SOS_low', 'SOS_trigger', 'default', 'N', '0');
-
-
-
